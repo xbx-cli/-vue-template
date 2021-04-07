1ab3f98a959e7a6e8f591f381bd935afb170b281
xiaoliuzhuan <zgqlnione@gmail.com>
2021-04-07 11:40:08 +0800

    333

---
+++ ask.js
@@ -0,0 +1,21 @@
+module.exports = [{
+        type: 'confirm',
+        name: 'private',
+        message: 'ths resgistery is private11111?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'author22222?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'description33333?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'license44444?',
+    },
+]
