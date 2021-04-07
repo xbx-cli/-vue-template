2da58c184f59d00fd11d44550f4ee433362fb637
xiaoliuzhuan <zgqlnione@gmail.com>
2021-04-07 13:22:42 +0800

    555

--- ask.js
+++ ask.js
@@ -3,19 +3,10 @@ module.exports = [{
         name: 'private',
         message: 'ths resgistery is private11111?',
     },
-    {
-        type: 'input',
-        name: 'author',
-        message: 'author22222?',
-    },
+
     {
         type: 'input',
         name: 'description',
         message: 'description33333?',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'license44444?',
-    },
+    }
 ]
